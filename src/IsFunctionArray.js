eeedc72d2ce09ec1c2436fce16eb1d91609a9d00
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-22 00:08:29 +0200

    call isArrayOfFunctions method.

--- src/IsFunctionArray.js
+++ src/IsFunctionArray.js
@@ -1,6 +1,13 @@
 'use strict';
 import validator from "@euriklis/validator";
+/**
+ * 
+ * @param {Array.<function>} item 
+ * @description this utility function tests
+ * if the argument is an array with function
+ * type elements.
+ * @returns {boolean}
+ */
 const IsFunctionArray = item => new validator(item)
-    .is_array_and_for_every(element => element.isFunction)
-    .answer;
+    .isArrayOfFunctions.answer;
 export default IsFunctionArray;
