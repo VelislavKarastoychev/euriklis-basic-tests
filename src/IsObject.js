203e10c8c395f3eb76d5e21f9d74db89a876a68a
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-23 20:07:57 +0200

    checks if the parameter of the function is an arbitrary object.

--- src/IsObject.js
+++ src/IsObject.js
@@ -1,4 +1,11 @@
 'use strict';
 import validator from "@euriklis/validator";
+/**
+ * 
+ * @param {Object} item 
+ * @description this function checks if the parameter
+ * is an object javascript type.
+ * @returns {boolean}
+ */
 const IsObject = item => new validator(item).isObject.answer;
 export default IsObject;
