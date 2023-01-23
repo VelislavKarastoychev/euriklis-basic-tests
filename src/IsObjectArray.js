ae1249411232d9396b77fd1c440067b52ef37bc5
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-23 20:10:12 +0200

    checks if the argument of the function is array of objects.

--- src/IsObjectArray.js
+++ src/IsObjectArray.js
@@ -1,4 +1,11 @@
 'use strict';
 import validator from "@euriklis/validator";
+/**
+ * 
+ * @param {Array.<object>} item 
+ * @description this function checks if the
+ * parameter is an array of object elements.
+ * @returns {boolean}
+ */
 const IsObjectArray = item => new validator(item).isObjectArray.answer;
 export default IsObjectArray;
