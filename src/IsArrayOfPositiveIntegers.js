af8c60bc7a70714ec9418b734dd24cff83d56e44
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-23 17:10:52 +0200

    Tests if the argument of the function is array of positive integers.

--- src/IsArrayOfPositiveIntegers.js
+++ src/IsArrayOfPositiveIntegers.js
@@ -1,4 +1,12 @@
 'use strict';
 import validator from '@euriklis/validator';
+/**
+ * 
+ * @param {Array.<number>} item 
+ * @description this utility function tests
+ * if the argument is an array with elements
+ * that are positive INTEGERS.
+ * @returns {boolean}
+ */
 const IsArrayOfPositiveIntegers = item => new validator(item).isArrayOfPositiveIntegers.answer;
 export default IsArrayOfPositiveIntegers;
