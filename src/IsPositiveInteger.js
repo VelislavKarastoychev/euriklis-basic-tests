823e1fceefe74eb0011d3fdaafc65ce9f3d3ad53
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-23 20:05:55 +0200

    Checks if the argument is a positive integer.

--- src/IsPositiveInteger.js
+++ src/IsPositiveInteger.js
@@ -1,4 +1,11 @@
 'use strict';
 import validator from '@euriklis/validator';
+/**
+ * 
+ * @param {number} item 
+ * @description this function checks if the parameter
+ * of the function is positive integer number.
+ * @returns {boolean}
+ */
 const IsPositiveInteger = item => new validator(item).isPositiveInteger.answer;
 export default IsPositiveInteger;
