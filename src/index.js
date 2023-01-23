837a386d9605a7780aaabdb2311775534c8aa6aa
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-24 00:21:55 +0200

    Collection of all the condition functions.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 'use strict';
+/**
+ * @description This file is a collection of
+ * all the functionalities of the package.
+ */
 export { default as IsArray } from './IsArray.js';
 export { default as IsArrayOfNegativeIntegers } from './IsArrayOfNegativeIntegers.js';
 export { default as IsArrayOfPositiveIntegers } from './IsArrayOfPositiveIntegers.js';
+export { default as IsArrayOfNegativeNumbers } from './IsArrayOfNegativeNumbers.js';
 export { default as IsArrayOfPositiveNumbers } from './IsArrayOfPositiveNumbers.js';
 export { default as IsArrayWithLength } from './IsArrayWithLength.js';
 export { default as IsArrayWithLengthBiggerThan } from './IsArrayWithLengthBiggerThan.js';
