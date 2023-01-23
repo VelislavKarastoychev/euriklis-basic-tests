046894a0366066a1db46ba1eb8a9f0cd4f050dda
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-23 23:24:13 +0200

    checks if the argument is integer array.

--- src/IsIntegerArray.js
+++ src/IsIntegerArray.js
@@ -1,4 +1,11 @@
 'use strict';
 import validator from "@euriklis/validator";
+/**
+ * 
+ * @param {Array.<number>} item 
+ * @description this function checks if the
+ * argument is an array of integers.
+ * @returns {boolean}
+ */
 const IsIntegerArray = item => new validator(item).isIntegerArray.answer;
 export default IsIntegerArray;
