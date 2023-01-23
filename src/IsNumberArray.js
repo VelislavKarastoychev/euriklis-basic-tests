6873c0d5eff5101105f000fc0e69cde550aa7ab4
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-23 20:15:40 +0200

    tests if the parameter of the argument is an array of arbitrary numbers.

--- src/IsNumberArray.js
+++ src/IsNumberArray.js
@@ -1,4 +1,11 @@
 'use strict';
 import validator from "@euriklis/validator";
+/**
+ * 
+ * @param {Array.<number>} item 
+ * @description this function checks if the argument is
+ * an array of arbitrary numbers.
+ * @returns {boolean}
+ */
 const IsNumberArray = item => new validator(item).isNumberArray.answer;
 export default IsNumberArray;
