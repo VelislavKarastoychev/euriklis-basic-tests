9a284bd37a940b2202e5046f66006873996449da
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-23 23:38:58 +0200

    Checks if the argument is an array with length equals or bigger than the second argument.

--- src/IsArrayWithLengthBiggerOrEqualTo.js
+++ src/IsArrayWithLengthBiggerOrEqualTo.js
@@ -1,5 +1,14 @@
 'use strict';
 import validator from "@euriklis/validator";
+/**
+ * 
+ * @param {Array} array 
+ * @param {number} length 
+ * @description this function checks if the argument
+ * is an array which length is equals or bigger than
+ * the length parameter (the second argument of the function).
+ * @returns {boolean}
+ */
 const IsArrayWithLengthBiggerOrEqualTo = (array, length) => {
     return new validator(array).isArray.And.has_length_equals_or_bigger_than(length).answer;
 }
