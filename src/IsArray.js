2f1554e88e8c0a90e095b933940a4228639a42ff
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-23 23:43:07 +0200

    tests if the argument is an array type.

--- src/IsArray.js
+++ src/IsArray.js
@@ -1,4 +1,11 @@
 'use strict';
 import validator from "@euriklis/validator";
+/**
+ * 
+ * @param {Array} item 
+ * @description this function checks if the argument
+ * is an array.
+ * @returns {boolean}
+ */
 const IsArray = item => new validator(item).isArray.answer;
 export default IsArray;
