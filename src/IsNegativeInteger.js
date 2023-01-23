0a1b5fed8ed29af3e5212413440ab22ffa8085a9
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-23 23:20:15 +0200

    Checks if the argument is a negative integer.

--- src/IsNegativeInteger.js
+++ src/IsNegativeInteger.js
@@ -1,4 +1,11 @@
 'use strict';
 import validator from '@euriklis/validator';
+/**
+ * 
+ * @param {number} item 
+ * @description this function checks if the argument
+ * is a negative integer.
+ * @returns {boolean}
+ */
 const IsNegativeInteger = item => new validator(item).isNegativeInteger.answer;
 export default IsNegativeInteger;
