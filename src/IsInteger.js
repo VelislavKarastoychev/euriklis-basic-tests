f5de42b96f205770feffa32a84b5762643d6b708
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-23 23:22:09 +0200

    tests if the argument is integer.

--- src/IsInteger.js
+++ src/IsInteger.js
@@ -1,4 +1,11 @@
 'use strict';
 import validator from "@euriklis/validator";
+/**
+ * 
+ * @param {number} item 
+ * @description this function checks if the argument
+ * is an integer.
+ * @returns {boolean}
+ */
 const IsInteger = item => new validator(item).isInteger.answer;
 export default IsInteger;
