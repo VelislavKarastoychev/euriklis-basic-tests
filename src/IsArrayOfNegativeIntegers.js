d31cf14535ed5f837ddc5d62b4640c8fd30b57e1
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-23 23:40:59 +0200

    tests if the argument is an array of negative integers.

--- src/IsArrayOfNegativeIntegers.js
+++ src/IsArrayOfNegativeIntegers.js
@@ -1,4 +1,11 @@
 'use strict';
 import validator from "@euriklis/validator";
+/**
+ * 
+ * @param {Array.<number>} item 
+ * @description this function checks if the argument
+ * is an array of negative integers.
+ * @returns {boolean}
+ */
 const IsArrayOfNegativeIntegers = item => new validator(item).isArrayOfNegativeIntegers.answer;
 export default IsArrayOfNegativeIntegers;
