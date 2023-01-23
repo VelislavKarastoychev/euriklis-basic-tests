e2c1306d7fae926b4a6b05e64eaa47e95de8639a
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-23 23:35:38 +0200

    tests if the argument is array in a closed range described from the second and the third parameter.

--- src/IsArrayWithLengthInClosedRange.js
+++ src/IsArrayWithLengthInClosedRange.js
@@ -1,5 +1,17 @@
 'use strict';
 import validator from '@euriklis/validator';
+/**
+ * 
+ * @param {Array} array 
+ * @param {number} lower 
+ * @param {number} upper 
+ * @description this function checks if the 
+ * argument is an array and if its length/size
+ * lies in the closed interval [lower, upper],
+ * where the lower and upper are the second
+ * and the third argument of the function.
+ * @returns {boolean}
+ */
 const IsArrayWithLengthInClosedRange = (array, lower, upper) => {
     return new validator(array).isArray
     .And
