b73b51f6d34a5c82d03505133d6db577258493bd
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-23 23:30:08 +0200

    checks if the argument is empty array/string/object/undefined/null.

--- src/IsEmpty.js
+++ src/IsEmpty.js
@@ -1,4 +1,12 @@
 'use strict';
 import validator from "@euriklis/validator";
+/**
+ * 
+ * @param {any} item 
+ * @description this function checks if the
+ * argument is an empty array or string or object
+ * or is undefined.
+ * @returns {boolean}
+ */
 const IsEmpty = item => new validator(item).isEmpty.answer;
 export default IsEmpty;
