c4e4fa4eb236a853b0fe6240dd7d1de6bc863188
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-23 19:28:06 +0200

    checks if the underlined argument of the function is undefined javascript type.

--- src/IsUndefined.js
+++ src/IsUndefined.js
@@ -1,4 +1,11 @@
 'use strict';
 import validator from "@euriklis/validator";
+/**
+ * 
+ * @param {any} item 
+ * @description this function checks if the argument
+ * of is undefined javascript type.
+ * @returns {boolean}
+ */
 const IsUndefined = item => new validator(item).isUndefined.answer;
 export default IsUndefined;
