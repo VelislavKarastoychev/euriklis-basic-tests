8f56c4c7441452fc81d8de806c846cebfc697fa3
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-23 23:31:39 +0200

    checks if the argument is a boolean js type.

--- src/IsBoolean.js
+++ src/IsBoolean.js
@@ -1,4 +1,11 @@
 'use strict';
 import validator from "@euriklis/validator";
+/**
+ * 
+ * @param {boolean} item 
+ * @description this function checks if the argument
+ * is a boolean javascript type.
+ * @returns {boolean}
+ */
 const IsBoolean = item => new validator(item).isBoolean.answer;
 export default IsBoolean;
