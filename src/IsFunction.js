ff4084a34297422be028c5211d5a64d8766e168c
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-23 23:26:16 +0200

    checks if the argument is a function.

--- src/IsFunction.js
+++ src/IsFunction.js
@@ -1,4 +1,12 @@
 'use strict';
 import validator from "@euriklis/validator";
+/**
+ * 
+ * @param {function} item 
+ * @description this function checks if the
+ * current argument is a legal javascript function
+ * type.
+ * @returns {boolean}
+ */
 const IsFunction = item => new validator(item).isFunction.answer;
 export default IsFunction;
