68d43b8567702a78b459d4c6b307d946518771d0
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-23 19:41:29 +0200

    Checks if a string has length which lies in a closed interval formed from the other two arguments of the function.

--- src/IsStringWithLengthInClosedRange.js
+++ src/IsStringWithLengthInClosedRange.js
@@ -1,5 +1,16 @@
 'use strict';
 import validator from '@euriklis/validator';
+/**
+ * 
+ * @param {string} str 
+ * @param {number} lower 
+ * @param {number} upper 
+ * @description this function checks if the 
+ * first argument is string which lies in a
+ * closed interval [lower, upper] which is formed
+ * from the other two arguments.
+ * @returns {boolean}
+ */
 const IsStringWithLengthInClosedRange = (str, lower, upper) => {
     return new validator(str).isString
         .And
