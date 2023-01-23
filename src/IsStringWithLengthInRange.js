f5853b05a31b52cd86504ebe341c17483fa1e581
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-23 19:37:19 +0200

    tests if some string element has length which lies in an open interval.

--- src/IsStringWithLengthInRange.js
+++ src/IsStringWithLengthInRange.js
@@ -1,5 +1,15 @@
 'use strict';
 import validator from '@euriklis/validator';
+/**
+ * 
+ * @param {string} str 
+ * @param {number} lower 
+ * @param {number} upper 
+ * @description this function checks if the 
+ * first argument is in the range formed from
+ * the other two arguments.
+ * @returns {boolean}
+ */
 const IsStringWithLengthInRange = (str, lower, upper) => {
     return new validator(str).isString
         .And
