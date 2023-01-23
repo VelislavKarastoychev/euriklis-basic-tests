e31abd56276679c80665a1d2427c648015e950c9
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-23 23:27:52 +0200

    tests if the current parameter is a floating point number.

--- src/IsFloat.js
+++ src/IsFloat.js
@@ -1,4 +1,11 @@
 'use strict';
 import validator from "@euriklis/validator";
+/**
+ * 
+ * @param {number} item 
+ * @description this function checks if the argument
+ * is a floating point number in javascript.
+ * @returns {boolean}
+ */
 const IsFloat = item => new validator(item).isFloat.answer;
 export default IsFloat;
