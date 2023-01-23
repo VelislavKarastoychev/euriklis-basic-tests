03c215bf9a4ba6bef21218b1650b3bab08ec5aaf
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-23 20:03:43 +0200

    tests if the argument is array of string elements.

--- src/IsStringArray.js
+++ src/IsStringArray.js
@@ -1,4 +1,11 @@
 'use strict';
 import validator from "@euriklis/validator";
+/**
+ * 
+ * @param {Array.<string>} item 
+ * @description this function is tests if the 
+ * argument is array of string elements.
+ * @returns {boolean}
+ */
 const IsStringArray = item => new validator(item).isStringArray.answer;
 export default IsStringArray;
