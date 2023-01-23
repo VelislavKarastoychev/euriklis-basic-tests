9bb4a33813cfa329c0b60551d9e4527ae3a71352
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-23 20:13:05 +0200

    checks if the argument of the function is an arbitrary number.

--- src/IsNumber.js
+++ src/IsNumber.js
@@ -1,4 +1,11 @@
 'use strict';
 import validator from "@euriklis/validator";
+/**
+ * 
+ * @param {number} item 
+ * @description this function checks if the argument
+ * is an arbitrary number.
+ * @returns {boolean}
+ */
 const IsNumber = item => new validator(item).isNumber.answer;
 export default IsNumber;
