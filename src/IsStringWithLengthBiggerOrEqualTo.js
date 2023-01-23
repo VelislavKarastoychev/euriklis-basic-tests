c411215c87db4c53d0bd986b964d6f494f4a5c59
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-23 19:58:39 +0200

    Checks if the strng is legth bigger or equals to a number.

--- src/IsStringWithLengthBiggerOrEqualTo.js
+++ src/IsStringWithLengthBiggerOrEqualTo.js
@@ -1,5 +1,14 @@
 'use strict';
 import validator from "@euriklis/validator";
+/**
+ * 
+ * @param {string} str 
+ * @param {number} length 
+ * @description this function tests if the
+ * first argument is string with length bigger
+ * or equal to the second argument.
+ * @returns {boolean}
+ */
 const IsStringWithLengthBiggerOrEqualTo = (str, length) => {
     return new validator(str).isString.And.has_length_equals_or_bigger_than(length).answer;
 }
