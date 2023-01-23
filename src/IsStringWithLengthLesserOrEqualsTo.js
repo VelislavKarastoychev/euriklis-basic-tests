872765f76bda6544cfdec0de1901891f1e0781da
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-23 19:33:30 +0200

    Checks if the string is with length equal or less than the second argument.

--- src/IsStringWithLengthLesserOrEqualsTo.js
+++ src/IsStringWithLengthLesserOrEqualsTo.js
@@ -1,5 +1,14 @@
 'use strict';
 import validator from "@euriklis/validator";
+/**
+ * 
+ * @param {string} str 
+ * @param {number} length 
+ * @description this function checks if the first
+ * argument is string with length equals to the length
+ * (the second argument).
+ * @returns {boolean}
+ */
 const IsStringWithLengthLesserOrEqualsTo = (str, length) => {
     return new validator(str).isString.And.has_length_equals_or_lesser_than(length).answer;
 }
