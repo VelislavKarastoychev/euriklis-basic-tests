41ca80213a5e46a593b38705f89b2d1abad167b9
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-23 20:01:13 +0200

    Tests if the parameter of the function is  a string.

--- src/IsString.js
+++ src/IsString.js
@@ -1,4 +1,11 @@
 'use strict';
 import validator from "@euriklis/validator";
+/**
+ * 
+ * @param {string} item 
+ * @description this function checks if the argument
+ * is a string.
+ * @returns {boolean}
+ */
 const IsString = item => new validator(item).isString.answer;
 export default IsString;
