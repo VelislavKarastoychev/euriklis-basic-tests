cac5f897538f14c2ac0dc1ec7acd40fab4168cc7
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-24 23:02:07 +0200

    tests if the argument is an array of string or number elements.

---
+++ src/IsArrayOfStringOrNumberElements.js
@@ -0,0 +1,53 @@
+'use strict';
+import validator from '@euriklis/validator';
+const IsArrayOfStringOrNumberElements = array => {
+    if (new validator(array).isArray.answer) {
+        const n = array.length;
+        let i, j, answer = true;
+        if (n === 0) answer = false;
+        else {
+            for (i = 0; i < n >> 2; i++) {
+                j = i << 2;
+                if (typeof array[j] !== 'string' || typeof array[j] !== 'number') {
+                    answer = false;
+                    break
+                } 
+                ++j;
+                if (typeof array[j] !== 'string' || typeof array[j] !== 'number') {
+                    answer = false;
+                    break
+                }
+                ++j;
+                if (typeof array[j] !== 'string' || typeof array[j] !== 'number') {
+                    answer = false;
+                    break
+                }
+                ++j;
+                if (typeof array[j] !== 'string' || typeof array[j] !== 'number') {
+                    answer = false;
+                    break
+                }
+            }
+            if (answer && (n % 4 >= 3)) {
+                j = n - 3;
+                if (typeof array[j] !== 'string' || typeof array[j] !== 'number') {
+                    answer = false;
+                }
+            }
+            if (answer && (n % 4 >= 2)) {
+                j = n - 2;
+                if (typeof array[j] !== 'string' || typeof array[j] !== 'number') {
+                    answer = false;
+                }
+            }
+            if (answer && (n % 4 >= 1)) {
+                j = n - 1;
+                if (typeof array[j] !== 'string' || typeof array[j] !== 'number') {
+                    answer = false;
+                }
+            }
+        }
+    } else return false;
+    return answer;
+}
+export default IsArrayOfStringOrNumberElements;
