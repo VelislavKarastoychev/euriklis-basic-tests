a7715559dd5ee6c73020755e20c9a35cc1810328
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-24 23:00:18 +0200

    tests if the argument is an array with string or number elements.

---
+++ src/IsArrayOfStringOrObjectElements.js
@@ -0,0 +1,61 @@
+'use strict';
+import validator from "@euriklis/validator";
+/**
+ * 
+ * @param {Array.<string | object>} array 
+ * @description this function tests if the argument
+ * is an array, every element of which is string or
+ * object javascript type.
+ * @returns {boolean}
+ */
+const IsArrayOfStringOrObjectElements = (array) => {
+    if (new validator(array).isArray.answer) {
+        const n = array.length;
+        let i, j, answer = true;
+        if (n === 0) answer = false;
+        else {
+            for (i = 0; i < n >> 2; i++) {
+                j = i << 2;
+                if (typeof array[j] !== 'string' || Object.prototype.toString.call(array[j]) !== '[object Object]') {
+                    answer = false;
+                    break;
+                }
+                ++j;
+                if (typeof array[j] !== 'string' || Object.prototype.toString.call(array[j]) !== '[object Object]') {
+                    answer = false;
+                    break;
+                }
+                ++j;
+                if (typeof array[j] !== 'string' || Object.prototype.toString.call(array[j]) !== '[object Object]') {
+                    answer = false;
+                    break;
+                }
+                ++j;
+                if (typeof array[j] !== 'string' || Object.prototype.toString.call(array[j]) !== '[object Object]') {
+                    answer = false;
+                    break;
+                }
+            }
+            if (answer && n % 4 >= 3) {
+                j = n - 3;
+                if (typeof array[j] !== 'string' || Object.prototype.toString.call(array[j]) !== '[object Object]') {
+                    answer = false;
+                }
+            }
+            if (answer && n % 4 >= 2) {
+                j = n - 2;
+                if (typeof array[j] !== 'string' || Object.prototype.toString.call(array[j]) !== '[object Object]') {
+                    answer = false;
+                }
+            }
+            if (answer && n % 4 >= 1) {
+                j = n - 1;
+                if (typeof array[j] !== 'string' || Object.prototype.toString.call(array[j]) !== '[object Object]') {
+                    answer = false;
+                }
+            }
+        }
+        return answer;
+    } else return false;
+} 
+export default IsArrayOfStringOrObjectElements;
