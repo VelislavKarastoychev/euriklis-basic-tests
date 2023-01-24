5f70dea1eb918c0507d0363e8b95046ccafc5e15
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-24 23:14:18 +0200

    checks if the argument is an array of string or object elements.

--- src/IsArrayOfStringOrObjectElements.js
+++ src/IsArrayOfStringOrObjectElements.js
@@ -9,53 +9,54 @@ import validator from "@euriklis/validator";
  * @returns {boolean}
  */
 const IsArrayOfStringOrObjectElements = (array) => {
+    let answer = true;
     if (new validator(array).isArray.answer) {
         const n = array.length;
-        let i, j, answer = true;
+        let i, j;
         if (n === 0) answer = false;
         else {
             for (i = 0; i < n >> 2; i++) {
                 j = i << 2;
-                if (typeof array[j] !== 'string' || Object.prototype.toString.call(array[j]) !== '[object Object]') {
+                if (typeof array[j] !== 'string' && Object.prototype.toString.call(array[j]) !== '[object Object]') {
                     answer = false;
                     break;
                 }
                 ++j;
-                if (typeof array[j] !== 'string' || Object.prototype.toString.call(array[j]) !== '[object Object]') {
+                if (typeof array[j] !== 'string' && Object.prototype.toString.call(array[j]) !== '[object Object]') {
                     answer = false;
                     break;
                 }
                 ++j;
-                if (typeof array[j] !== 'string' || Object.prototype.toString.call(array[j]) !== '[object Object]') {
+                if (typeof array[j] !== 'string' && Object.prototype.toString.call(array[j]) !== '[object Object]') {
                     answer = false;
                     break;
                 }
                 ++j;
-                if (typeof array[j] !== 'string' || Object.prototype.toString.call(array[j]) !== '[object Object]') {
+                if (typeof array[j] !== 'string' && Object.prototype.toString.call(array[j]) !== '[object Object]') {
                     answer = false;
                     break;
                 }
             }
             if (answer && n % 4 >= 3) {
                 j = n - 3;
-                if (typeof array[j] !== 'string' || Object.prototype.toString.call(array[j]) !== '[object Object]') {
+                if (typeof array[j] !== 'string' && Object.prototype.toString.call(array[j]) !== '[object Object]') {
                     answer = false;
                 }
             }
             if (answer && n % 4 >= 2) {
                 j = n - 2;
-                if (typeof array[j] !== 'string' || Object.prototype.toString.call(array[j]) !== '[object Object]') {
+                if (typeof array[j] !== 'string' && Object.prototype.toString.call(array[j]) !== '[object Object]') {
                     answer = false;
                 }
             }
             if (answer && n % 4 >= 1) {
                 j = n - 1;
-                if (typeof array[j] !== 'string' || Object.prototype.toString.call(array[j]) !== '[object Object]') {
+                if (typeof array[j] !== 'string' && Object.prototype.toString.call(array[j]) !== '[object Object]') {
                     answer = false;
                 }
             }
         }
-        return answer;
-    } else return false;
-} 
+    } else answer = false;
+    return answer;
+}
 export default IsArrayOfStringOrObjectElements;
